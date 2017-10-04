a38c0b95af04d4584557ad056d697a0e08018b55
pragyan1994 <pragyanpatra.23@gmail.com>
2017-10-04 18:02:49 +0530

    Update run.py

--- qark/run.py
+++ qark/run.py
@@ -1,6 +1,8 @@
 import os
-x = os.environ['JENKINS_HOME']+'/jobs/'+os.environ['JOB_NAME']+'/workspace/qark/sampleApps/goatdroid/goatdroid.apk'
+x = os.environ['JENKINS_HOME']+'/jobs/'+os.environ['JOB_NAME']+'/workspace/qark/sampleApps/goatdroid/goatdroid.apk' 
+y = os.environ['JENKINS_HOME']+'/jobs/'+os.environ['JOB_NAME']+'/workspace'
 print x
-os.system("python qark/qarkMain.py --source 1 --pathtoapk "+x+" --exploit 1 --install 0 --reportdir /Users/suruchi.srivastava/.jenkins/jobs/qark/workspace");
+print y
+os.system("python qark/qarkMain.py --source 1 --pathtoapk "+x+" --exploit 1 --install 0 --reportdir" +y );
 
 print "Done"
